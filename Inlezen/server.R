acc1c2640fdf46af693ca5e35440b1ae921653d2
David <david.gies@starcom.nl>
2016-12-30 08:26:48 +0100

    Update server.R

--- Inlezen/server.R
+++ Inlezen/server.R
@@ -1,47 +1,35 @@
-#
-
 library(shiny)
-function(input, output) {
+shinyServer(function(input, output, session) {
+  # instelling om grotere bestanden in te kunnen lezen
   options(shiny.maxRequestSize=30*1024^2)
-  output$contents <- renderTable({
-
-    
-    inFile <- input$uploadFile 
+  
+  # de data wordt hier in het mainPanel getoond
+  datasetInput <- reactive({
+    inFile <- input$uploadFile
     
     if (is.null(inFile))
       return(NULL)
+    
     file.rename(inFile$datapath,
                 paste(inFile$datapath, ".xlsx", sep=""))
-
-    vec_types <- c("text", "text", "text", "text", "text", "text", "text", "text", "text", "text", "text",
-                   "numeric", "numeric", "numeric", "numeric", "text", "numeric", "text", "text", "text",
-                   "numeric", "numeric", "numeric", "numeric", "numeric", "text", "text", "text", "text", "text", "text", "text",
-                   "text", "text", "text", "text", "text", "text", "text")
-    LD <- readxl::read_excel(path = paste(inFile$datapath, ".xlsx", sep=""), sheet = 1, col_types = vec_types)
- #   LD$Work_Date <- as.Date(LD$Work_Date, origin = "1899-12-30")
-    # df <- list()
-    # df$VX <- openxlsx::read.xlsx(inFile$datapath, 1)
-    # df$VX <- dplyr::select(df$VX, 3)
-    
-
-    
-  })
-  output$contents2 <- renderTable({
-
-    inFile2 <- input$uploadFile2
-    if (is.null(inFile2))
-      return(NULL)
-    file.rename(inFile2$datapath,
-                paste(inFile2$datapath, ".xlsx", sep=""))
     
     vec_types <- c("text", "text", "text", "text", "text", "text", "text", "text", "text", "text", "text",
                    "numeric", "numeric", "numeric", "numeric", "text", "numeric", "text", "text", "text",
                    "text", "text", "text", "text", "text", "text", "text", "text", "text", "text", "text", "text",
                    "text", "text", "text", "text", "text", "text", "text")
-    LO <- readxl::read_excel(path = paste(inFile2$datapath, ".xlsx", sep=""), sheet = 1, col_types = vec_types)
-    LO$Created_Date <- as.Date(as.character(as.vector(unlist(LO$Created_Date))), origin = "1899-12-30")
-    #LO$Created_Date <- as.Date(LO$Created_Date, origin="1899-12-30")
-    #not_outlier_dates[,2]<-as.Date(as.numeric(as.vector(unlist(not_outlier_dates[,2]))))
-    
+    SC <- readxl::read_excel(path = paste(inFile$datapath, ".xlsx", sep=""), sheet = 1, col_types = vec_types)
   })
-}
+  output$contents <- renderTable(datasetInput())
+  
+  output$downloadData <- downloadHandler(
+    filename = function() { 
+      paste("werkbestand", '.csv', sep='')
+    },
+    content = function(file) {
+      write.csv(datasetInput(), file = file)   # file moet file blijven
+    }
+  )
+      session$onSessionEnded(function() {
+        stopApp()
+    })
+})
