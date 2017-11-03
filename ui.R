8e5717b3313ed3bc6aadb9204b80e08d10f5c660
David <david.gies@gmail.com>
2017-11-03 15:28:00 +0100

    Barebone

---
+++ ui.R
@@ -0,0 +1,18 @@
+library(shiny)
+library(shinythemes)
+# library(plotly)
+
+shinyUI(
+  navbarPage("Shiny", theme = shinytheme("lumen"), collapsible = TRUE, 
+             tabPanel("Test",  
+                      #shinythemes::themeSelector(),
+                      headerPanel("Test"),
+                      sidebarLayout(position = "left",
+                                    sidebarPanel(strong("Open ..."), br(),
+                                                 actionButton("knop_sc", "Browse...", width = '200px', style="color: #fff; background-color: #158cba; border-color: #158cba"), br(), br(), br()
+                                    ),
+                                    mainPanel(dataTableOutput('contents1'))
+                      )
+             )
+  )
+)
